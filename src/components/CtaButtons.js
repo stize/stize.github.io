f04b482f8d551781a1daca36b899e5efac134e2b
Sergio Moreno <sergio.m.calzada@gmail.com>
2020-10-14 00:17:48 +0200

    added azimuth gatsby template

---
+++ src/components/CtaButtons.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import _ from 'lodash';
+
+import {Link, withPrefix, classNames} from '../utils';
+
+export default class CtaButtons extends React.Component {
+    render() {
+        let actions = _.get(this.props, 'actions', null);
+        return (
+            _.map(actions, (action, action_idx) => (
+            <Link key={action_idx} to={withPrefix(_.get(action, 'url', null))}
+              {...(_.get(action, 'new_window', null) ? ({target: '_blank', rel: 'noopener'}) : null)}
+              className={classNames('button', {'secondary': _.get(action, 'primary', null) !== true})}>{_.get(action, 'label', null)}</Link>
+            ))
+        );
+    }
+}
